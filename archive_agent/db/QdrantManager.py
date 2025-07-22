14ffc1f4e98f3668377ddfca38221d71328a5297
shredEngineer <shredengineer@gmail.com>
2025-07-22 23:01:38 +0200

    v5.4.0
    Using OCR for PDF pages, entity extraction for images (fixed)

--- archive_agent/db/QdrantManager.py
+++ archive_agent/db/QdrantManager.py
@@ -1,7 +1,7 @@
 # TODO:
 #  - Implement Pydantic schema for point metadata.
 #  - Add "version" field for migration.
-#  - Add chunk "from_image" flag.
+#  - Add chunk "orign" string, like "isImage, AiVisionEntity" or "isPage, AiVisionOCR".
 
 #  Copyright © 2025 Dr.-Ing. Paul Wilhelm <paul@wilhelm.dev>
 #  This file is part of Archive Agent. See LICENSE for details.
